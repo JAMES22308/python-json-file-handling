21bccc96fb8e0303ad21d7bb313d8a7cc210ae79
NARUTO22308 <soriajames170@gmail.com>
2025-04-24 09:30:39 +1200

    uncomment

--- json-handling.py
+++ json-handling.py
@@ -420,8 +420,8 @@ def main():
         "2": "sign in ✍️",
         "3": "exit 🚪",
     }
-    # print('AI is talking....')
-    # speech("hello, i am friday, and this is authentication, press 1, to sign up, press 2, to sign in, and press 3, to exit")
+    print('AI is talking....')
+    speech("hello, i am friday, and this is authentication, press 1, to sign up, press 2, to sign in, and press 3, to exit")
     
 
 
