0fe0a3274f8f95b6474f4e42249f8d99fee45886
NARUTO22308 <soriajames170@gmail.com>
2025-04-25 14:02:37 +1200

    car game function

--- new_file.py
+++ new_file.py
@@ -1,23 +1,25 @@
-print("-----------CAR GAME-----------")
-running = False
-while True:
-    option = input('type start to start | type stop to stop | type exit to exit: ')
-    if option == 'start':
-        if running:
-            print('engine is already running')
-        else:
-            print('engine started')
-            running = True
-    elif option == 'stop':
-        if not running:
-            print("engine is already stopped")
-        else:
-            print('engine is stopped')
-            running = False
-    elif option == 'exit':
-        print('-----------GAME OVER-----------')
 
-    else:
-        print('try again')
+def car_game():
+    print("-----------CAR GAME-----------")
+    running = False
+    while True:
+        option = input('press 1 to start | press 2 to stop | press 3 to exit: ')
+        if option == '1':
+            if running:
+                print('engine is already running')
+            else:
+                print('engine started')
+                running = True
+        elif option == '2':
+            if not running:
+                print("engine is already stopped")
+            else:
+                print('engine is stopped')
+                running = False
+        elif option == '3':
+            print('-----------GAME OVER-----------')
+            break
+        else:
+            print('try again')
         
-
+car_game()
